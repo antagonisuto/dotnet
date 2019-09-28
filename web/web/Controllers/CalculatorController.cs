6838ac9aacff102fb5b4610fc578251b4cdbc67a
Marfa Lamova <invisible.kichi@gmail.com>
2019-09-28 12:40:04 +0600

    first commit

---
+++ web/web/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult PlusTen(int number)
+        {
+            ViewData["number"] = number;
+            ViewData["result"] = number + 10;
+            return View();
+        }
+    }
+}
---
+++ web/web/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult ShowMessage (string messages)
+        {
+
+            if (string.IsNullOrEmpty(messages))
+            {
+                ViewData["Message"] = "Message are empty";
+            }
+            else
+            {
+                ViewData["Message"] = messages;
+            }
+
+            return View();
+        }
+    }
+}
